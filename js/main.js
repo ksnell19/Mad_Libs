ac0feb60f54d1fdf77b6c498e03bfcb8f6a1957f
ksnell19 <ksnell@Kristins-MacBook-Pro.local>
2018-04-03 00:16:17 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,52 +1,44 @@
-// When eyes button is clicked, toggle laser class on brain
-$(".flash").click(function() {
-  $(".brain").toggleClass('laser');
+// List of prompts for the user
+var prompts = [
+	'Type your name',
+	'Type an adjective',
+	'Type a noun'
+   ];
+
+var answers=[];
+// Keep track of current prompt we're on
+var currentPrompt = 0;
+
+// A function that will call the next prompt
+var nextPrompt = function() {
+  //if there's no answer in the form
+  if (currentPrompt != 0){
+    answers.push($('input').val());
+  }
+	// if there is a next prompt
+	if (currentPrompt < prompts.length) {
+		// put first prompt in all html elements with class
+		$('.prompt').html(prompts[currentPrompt] +'<br><input type="text">');
+		// move the next prompt into variable currentPrompt
+		currentPrompt = currentPrompt + 1;
+	}
+	//or else if we're at the end of the array
+	else {
+		// put a new message into the html.
+		showFinal();
+	}
+}
+
+//puts user answers into HTML
+var showFinal = function() {
+$('.prompt').html('This is the story of <span class="fill">'+answers[0]+'</span> and the <span class="fill">'+answers[1]+'</span> <span class="fill">'+answers[2]+'</span>.');
+//and then hide the button
+$('button').hide();
+}
+// run nextPrompt function when button is clicked
+$('button').click(function() {
+	nextPrompt();
 });
 
-// When color button is clicked...
-$(".color").click(function() {
-  // assign each named color a random number 0-255
-  var red = Math.floor(Math.random() * 255);
-  var green = Math.floor(Math.random() * 255);
-  var blue = Math.floor(Math.random() * 255);
-
-  //Generate an RBGA value from red, green, blue
-  var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
-  //change bodys background to random colors
-  $("body").css("background", randomRGBA);
-  //Display the rgba values(alert/log?)
-  // console.log(randomRGBA);
-});
-
-//Moves for dance button click
-// $(".dance").click(function() {
-//     if($(this).hasClass("clicked")){
-//         $(this).removeClass("clicked");
-//
-//         $(".robot").animate({left: '+=200px'});
-//     }else{
-//         $(this).addClass("clicked");
-//
-//         $(".robot").animate({left: '-=200px'});
-//       }
-//   });
-
-$(document).ready(function(){
-  $(".dance").click(function(){
-    $(".robot").animate({left: '+=300px'},"slow");
-    $(".robot").animate({left: '-=300px'},"slow");
-    $(".robot").animate({left: '-=300px'},"slow");
-    $(".robot").animate({left: '+=300px'},"slow");
-    $(".robot").animate({left: '+=300px'},"slow");
-    $(".robot").animate({left: '-=300px'},"slow");
-    $(".robot").animate({left: '-=300px'},"slow");
-    $(".robot").animate({left: '+=300px'},"slow");
-    $(".robot").animate({left: '+=300px'},"slow");
-    $(".robot").animate({left: '-=300px'},"slow");
-  });
-});
-
-//music play
-// function play() {
-//   document.getElementsByTagName('iframe')[0].src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/197629761&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true";
-// };
+// Show the first prompt as soon as js loads
+nextPrompt();
